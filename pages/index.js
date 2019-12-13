cda8eedb7de6ddffdf262268398ef9887227f7f2
Imre Bartis <imre.bartis@siili.com>
2019-12-13 17:11:32 +0200

    fetch posts with getinitialprops

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
-const Index = () => (
-  <div>
-    <h1>Index page</h1>
-  </div>
-);
+import axios from "axios";
+
+const Index = ({ posts }) => {
+  return (
+    <div>
+      <h1>Index page</h1>
+      <ul>
+      {posts.map(post => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+      </ul>
+    </div>
+  );
+};
+
+Index.getInitialProps = async () => {
+  const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+
+  const { data } = res;
+  return { posts: data };
+};
 
 export default Index;
