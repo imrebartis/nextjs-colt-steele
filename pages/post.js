ac478f5aad2b2340ce3f38585b5ec78591b57674
Imre Bartis <imre.bartis@siili.com>
2019-12-18 14:58:17 +0200

    fetch comments

--- pages/post.js
+++ pages/post.js
@@ -1,9 +1,28 @@
-import {withRouter} from "next/router";
+import axios from "axios";
 
-const Post = props => <h1>you're looking at post #{props.router.query.id}</h1>;
+const Post = ({ id, comments }) => (
+  <div>
+    <h1>Comments for post #{id}</h1>
+    {comments.map(comment => (
+      <Comment {...comment} key={comment.id} />
+    ))}
+  </div>
+);
 
-// Post.getInitialProps = async ({ query }) => {
-//   return query;
-// };
+const Comment = ({email, body}) => (
+  <div>
+    <h5>{email}</h5>
+    <p>{body}</p>
+  </div>
+)
 
-export default withRouter(Post);
+Post.getInitialProps = async ({ query }) => {
+  const res = await axios.get(
+    `https://jsonplaceholder.typicode.com/comments?postId=${query.id}`
+  );
+
+  const { data } = res;
+  return { ...query, comments: data };
+};
+
+export default Post;
